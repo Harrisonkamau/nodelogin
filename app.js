8284c85a670026394bf1d5a7e7326a81ef00e030
Harrisonkamau <kamauharrison87@gmail.com>
2016-10-07 15:43:04 +0300

    set global variables for flash messsages

--- app.js
+++ app.js
@@ -61,4 +61,15 @@ app.use(expressValidator({
     }
 }));
 
+// connect flash
+app.use(flash);
+
+// Global variables for flash messages
+app.use(function(req, res, next){
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+})
+
 
