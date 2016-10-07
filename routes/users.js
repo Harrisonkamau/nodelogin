59258af1eae6baa4d31ddd8df85021e385738a20
Harrisonkamau <kamauharrison87@gmail.com>
2016-10-07 17:14:41 +0300

    register a user

--- routes/users.js
+++ routes/users.js
@@ -11,5 +11,17 @@ router.get('/login', function(req, res){
     res.render('login');
 });
 
+// Register user
+router.post('/register', function(req, res){
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+    
+    console.log(username);
+
+});
+
 
 module.exports = router;
