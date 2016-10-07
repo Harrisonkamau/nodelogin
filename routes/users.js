794ad5bc5a7e030dc79e10aa80c399f763fac47e
Harrisonkamau <kamauharrison87@gmail.com>
2016-10-07 17:17:20 +0300

    validate form

--- routes/users.js
+++ routes/users.js
@@ -19,7 +19,20 @@ router.post('/register', function(req, res){
     var password = req.body.password;
     var password2 = req.body.password2;
     
-    console.log(username);
+    // console.log(username);
+
+    // Form validation
+    req.checkBody('name', 'Name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+    var errors  = req.validationErrors();
+
+    if(errors){
+
+    }
 
 });
 
