ed941e95248d425dc5665f5364e7d2ab260d0855
Harrisonkamau <kamauharrison87@gmail.com>
2016-10-12 13:35:10 +0300

    add coupon to db

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var numbers = require('../controllers/numbers.js'); 
+// var app = express();
 
 // Get homepage
 router.get('/', ensureAuthenticated, function(req, res){
 	res.render('index');
 });
+
+// load the coupon page
+router.get('/coupon', ensureAuthenticated, function(req, res){
+	res.render('coupon');
+})
+
+// add the invite code
+router.post('/coupon', ensureAuthenticated, numbers.add);
 
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
